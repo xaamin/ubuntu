59dbf13ee4469876f31a74672357c2c08af2923c
Benjamín Martínez Mateos <xaamin@outlook.com>
2016-09-23 11:41:27 -0500

    Fixed missing comment

--- root/sources.sh
+++ root/sources.sh
@@ -1,5 +1,5 @@
 # Git
-echo "Git" >> $HOME/.bashrc
+echo "# Git" >> $HOME/.bashrc
 
 echo "source $HOME/.scripts/git-completion.sh" >> $HOME/.bashrc
 echo "source $HOME/.scripts/git-prompt.sh" >> $HOME/.bashrc
