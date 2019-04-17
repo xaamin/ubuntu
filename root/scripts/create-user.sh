24d1afed01cda5f6d1cf736921edb9d10d90802f
Benjamín Martínez Mateos <xaamin@outlook.com>
2019-04-17 13:42:29 -0500

    Update scripts

--- root/scripts/create-user.sh
+++ root/scripts/create-user.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 
-USER=${USER:-'xaamin'}
+USER=${USER:-xaamin}
 
-USER_UID=${USER_UID:-8276}
+USER_ID=${USER_ID:-8276}
 USER_GID=${USER_GID:-8276}
 
 echo "Validating ${USER}"
 
+APPLY_PERMISSIONS=false
+
 if [[ ! -d "/home/$USER" ]]; then
+    APPLY_PERMISSIONS=true
+fi
+
+if [[ ! -d "/home/$USER" ]]; then
+    echo "Creating user $USER with uid $USER_ID guid $USER_GID"
+
     useradd -m $USER
 
     usermod -s /bin/false $USER
 
     cp /root/.bashrc /home/${USER}/.bashrc
+fi
 
-    echo "Creating user $USER with uid $USER_UID guid $USER_GID"
+/bin/bash /root/scripts/fix-uid-and-gid.sh
 
-    usermod -o -u $USER_UID $USER
-    groupmod -o -g $USER_GID $USER
-
-    echo '
-    -------------------------------------
-    GID/UID
-    -------------------------------------'
-    echo "
-    User uid:    $(id -u $USER)
-    User gid:    $(id -g $USER)
-    -------------------------------------
-    "
+if $APPLY_PERMISSIONS ; then
+    echo "Applying permissions to home"
 
     chown -R $USER:$USER "/home/$USER"
-else
-    echo '
+
+    chmod g+rwxs "/home/$USER"
+fi
+
+echo '
     -------------------------------------
-    GID/UID
+    Finished with GID/UID
     -------------------------------------'
-    echo "
+echo "
     User uid:    $(id -u $USER)
     User gid:    $(id -g $USER)
     -------------------------------------
     "
-fi
 
 ls -la /home
--- root/scripts/start-ssh.sh
+++ root/scripts/start-ssh.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Create the default user
+/bin/bash /root/scripts/create-user.sh
+
+# Configure SSH service
 /bin/bash /root/scripts/config-ssh.sh
 
+# Start the SSH service foreground
 /usr/sbin/sshd -D
